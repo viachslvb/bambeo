aa81468037d6b2a54d8d0466167e0eec87d10494
Vyacheslav <viacheslav.borys@gmail.com>
2024-08-18 23:01:59 +0200

    Enhance cookie consent UI

--- Bambeo/src/app/core/components/cookie-notice/cookie-notice.component.ts
+++ Bambeo/src/app/core/components/cookie-notice/cookie-notice.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { fadeOutAnimation } from '../../animations';
 
-const COOKIE_CONSENT_VERSION = '1.0';
+const COOKIE_CONSENT_VERSION = '1.1';
 
 @Component({
   selector: 'app-cookie-notice',
